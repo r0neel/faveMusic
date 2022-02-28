f3b2ad9308a1b6cb88cb37bc2c840dd02a021344
r0neel <ronbhagwakar@gmail.com>
2022-02-28 13:00:40 +0000

    init commit

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,12 @@
+const config = require("./webpack.config.js");
+
+config.devServer = {
+  historyApiFallback: true, //serve a previous page on a 404 error
+  port: 8080, // use this port for the server
+  open: true, // open the project in the browser when the server starts
+  liveReload: true // refresh the browser when changes are saved
+};
+
+config.devtool = "inline-source-map"; // a tool to find errors in the compiled code, but show them against the source code for easier debugging
+
+module.exports = config;
